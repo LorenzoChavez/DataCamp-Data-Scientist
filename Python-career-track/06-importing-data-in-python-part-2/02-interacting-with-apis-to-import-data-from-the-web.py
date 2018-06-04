5535dc02f12f878d99c8dc0ee1df3ad5fdfdbab8
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-06-04 22:47:28 +0200

    added last course

---
+++ Python-career-track/06-importing-data-in-python-part-2/02-interacting-with-apis-to-import-data-from-the-web.py
@@ -0,0 +1,55 @@
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+url = "http://www.omdbapi.com/?apikey=ff21610b&t=social+network"
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)
+
+
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=ff21610b&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza"
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
+
---
+++ Python-career-track/06-importing-data-in-python-part-2/03-diving-deep-into-the-twitter-api.py
@@ -0,0 +1,86 @@
+# Import package
+import tweepy
+
+# Store OAuth authentication credentials in relevant variables
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create you Stream object with authentication
+stream = tweepy.Stream(auth, 1)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=["clinton", "trump", "sanders", "cruz"])
+
+# Import package
+import json
+
+# String of path to file: tweets_data_path
+tweets_data_path = "tweets.txt"
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data = []
+
+# Open connection to file
+tweets_file = open(tweets_data_path, "r")
+
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+
+# Close connection to file
+tweets_file.close()
+
+# Print the keys of the first tweet dict
+print(tweets_data[0].keys())
+
+
+# Import package
+import pandas as pd
+
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=["text", "lang"])
+
+# Print head of DataFrame
+print(df.head())
+
+
+
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text("trump", row["text"])
+    sanders += word_in_text("sanders", row["text"])
+    cruz += word_in_text("cruz", row["text"])
+
+
+# Import packages
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
+
