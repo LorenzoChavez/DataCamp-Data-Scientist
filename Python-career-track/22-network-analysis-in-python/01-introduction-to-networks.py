84faa72e2b8ed0ba3e0bb4daa616254a6953cdcf
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-08-30 00:02:46 +0200

    added network analysis course

---
+++ Python-career-track/22-network-analysis-in-python/01-introduction-to-networks.py
@@ -0,0 +1,110 @@
+# Import necessary modules
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Draw the graph to screen
+nx.draw(T_sub)
+plt.show()
+
+
+# Use a list comprehension to get the nodes of interest: noi
+noi = [n for n, d in T.nodes(data=True) if d['occupation'] == 'scientist']
+
+# Use a list comprehension to get the edges of interest: eoi
+eoi = [(u, v) for u, v, d in T.edges(data=True) if d["date"] < date(2010, 1, 1)]
+
+
+# Set the weight of the edge
+T.edge[1][10]['weight'] = 2
+
+# Iterate over all the edges (with metadata)
+for u, v, d in T.edges(data=True):
+
+    # Check if node 293 is involved
+    if 293 in [u, v]:
+    
+        # Set the weight to 1.1
+        T.edge[u][v]['weight'] = 1.1
+
+
+# Define find_selfloop_nodes()
+def find_selfloop_nodes(G):
+    """
+    Finds all nodes that have self-loops in the graph G.
+    """
+    nodes_in_selfloops = []
+    
+    # Iterate over all the edges of G
+    for u, v in G.edges():
+    
+    # Check if node u and node v are the same
+        if u == v:
+        
+            # Append node u to nodes_in_selfloops
+            nodes_in_selfloops.append(u)
+            
+    return nodes_in_selfloops
+
+# Check whether number of self loops equals the number of nodes in self loops
+assert T.number_of_selfloops() == len(find_selfloop_nodes(T))
+
+
+# Import nxviz
+import nxviz as nv
+
+# Create the MatrixPlot object: m
+m = nv.MatrixPlot(T)
+
+# Draw m to the screen
+m.draw()
+
+# Display the plot
+plt.show()
+
+# Convert T to a matrix format: A
+A = nx.to_numpy_matrix(T)
+
+# Convert A back to the NetworkX form as a directed graph: T_conv
+T_conv = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
+
+# Check that the `category` metadata field is lost from each node
+for n, d in T_conv.nodes(data=True):
+    assert 'category' not in d.keys()
+
+
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import CircosPlot
+
+# Create the CircosPlot object: c
+c = CircosPlot(T)
+
+# Draw c to the screen
+c.draw()
+
+# Display the plot
+plt.show()
+
+
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import ArcPlot
+
+# Create the un-customized ArcPlot object: a
+a = ArcPlot(T)
+
+# Draw a to the screen
+a.draw()
+
+# Display the plot
+plt.show()
+
+# Create the customized ArcPlot object: a2
+a2 = ArcPlot(T, node_order="category", node_color="category")
+
+# Draw a2 to the screen
+a2.draw()
+
+# Display the plot
+plt.show()
+
